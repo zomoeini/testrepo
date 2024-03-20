58621fa304e84fb1a84fc293b85daeb5a007e91f
Zohreh Moeini <38110795+zomoeini@users.noreply.github.com>
2024-03-20 09:36:47 -0400

    Add C# File

---
+++ CodeFile1.cs
@@ -0,0 +1,36 @@
+﻿/*using System.Collections.Generic;
+
+ZOO search in the Z
+
+
+Mary 	514 111 1111(Value)
+(key)m@email.com(Value)
+
+
+John 514 222 2222(Value)
+(key)   j @email.com(Value)
+
+
+Laura 514 333 3333(Value)
+(key)   L @email.com(Value)
+
+
+If I want to get the email address fro John 
+
+
+array[] { Mary, John, Laura}
+
+List<> { Object1, Object2,Object3}
+
+List<string, string> { Mary(value),m @email.com(value)}
+{ John(value),j @email.com(value)}
+{ Laura,L @email.com}
+I can not do the search sort delete edit based on the key 
+
+Dictionary<string,string> {[Mary, m@email.com],[John, j@email.com],[Laura, L@email.com]
+element in dictionary divided to Key and Value (first data is key ) (second is value)
+
+But in dictionary I can do the search based on the key 
+Mary is a key and I can access to the all information related to mary 
+I have Methods, functions  based on the key 
+*/
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemoClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a dictionary with string key (student name) and int for grades (Values)
+            //Dictionary<TKey, TValue>
+            Dictionary<string,int> studentGrades = new Dictionary<string, int>();
+
+            // Add some student names and grades to the dictionary 
+            studentGrades.Add("David", 98);
+            studentGrades.Add("Sarah", 79);
+            studentGrades.Add("Eve", 56);
+            studentGrades["Rose"] = 100;
+            studentGrades["Bob"] = 87;
+            studentGrades["Helen"] = 67;
+
+
+            // Display the contents of the dictionary 
+            Console.WriteLine("Student Grade :");
+
+            foreach(var student in studentGrades)
+            {
+                Console.WriteLine(student.Key + " "+ student.Value);
+            }
+
+            //TO DO 
+            // Course iformation with string string courseCode courseTitle
+
+
+            var cities = new Dictionary<string, string>()
+            {
+                {"Canada","ottawa,Montreal, Vancouver" },
+                {"USA", "Chicago, New york, Washington" },
+                {"India", "Mumbai, New Dehli, Pune" },
+                {"France", "Paris, Leon, Nice" }
+            };
+
+
+            //cities["USA"] = "Los Angles";
+            // the key is always unique 
+            Console.WriteLine("=====================================");
+            // I can show the list of cities based on the key value
+            Console.WriteLine(cities["Canada"]);
+            Console.WriteLine(cities["USA"]);
+            Console.WriteLine(cities["India"]);
+
+
+            // I want to check and see the key France is exist or not 
+
+            if(cities.ContainsKey("France"))
+            {
+                Console.WriteLine(cities["France"]);
+            }
+            else
+            {
+                Console.WriteLine("Key does not exist");
+            }
+
+            // I want to get the value for  key 
+            string result;
+
+            if(cities.TryGetValue("France", out result))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("value does not exist");
+            }
+
+            // use ElemetAt to retrive (key-Value) pair 
+
+            for(int i= 0; i< cities.Count; i++)
+            {
+                Console.WriteLine("key: {0}, Value: {1}", cities.ElementAt(i).Key, cities.ElementAt(i).Value);
+            }
+
+            foreach(KeyValuePair<string,string> pair in cities)
+            {
+                Console.WriteLine("key: {0}, Value: {1}", pair.Key, pair.Value);
+            }
+
+
+            // TO DO 
+            // Country and provinces or states 
+            // canada , USA , your own country 
+
+            Console.WriteLine("List of provinces for all countries ");
+
+            Console.WriteLine("Check the key Japon");
+
+            Console.WriteLine("user tryGetValue for Japon");
+
+            Console.WriteLine("ElementAt key-value pair");
+
+            Console.WriteLine("KeyValuePair key-value pair");
+
+            Console.ReadKey();
+
+        }
+    }
+}
